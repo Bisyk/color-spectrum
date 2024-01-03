089c9c59225e44ddbc0eb7ca3200af578b4397ca
Bisyk <bskyrslv@gmail.com>
2024-01-03 21:36:18 +0200

    Code formatting

--- src/ui/buttons/ColorButton.jsx
+++ src/ui/buttons/ColorButton.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const ColorButton = ({ color, backgroundButtonState, setPrimaryColor, setSecondaryColor }) => {
   const handleClick = () => {
     if (backgroundButtonState === 'active') {
